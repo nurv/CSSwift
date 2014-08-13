ae88b560fb2386afe24320445cfbf7f5b43e9ce5
Artur Ventura <artur.ventura@gmail.com>
2014-08-13 23:51:40 +0100

    Initial Commit

---
+++ CSSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CSSwift
+//
+//  Created by Artur Ventura on 13/08/14.
+//  Copyright (c) 2014 Artur Ventura. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
